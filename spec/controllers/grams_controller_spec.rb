5b0d26c0fe10be58729ed15b54b3b0eae1052ca5
Sloane <sloanems@aol.com>
2019-10-02 20:23:12 +0000

    factorybot gem

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -16,12 +16,9 @@ RSpec.describe GramsController, type: :controller do
       expect(response).to redirect_to new_user_session_path
     end
     it "should successfully show the new form" do
-     user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
+      
       get :new
       expect(response).to have_http_status(:success)
 
@@ -35,11 +32,7 @@ RSpec.describe GramsController, type: :controller do
       end
 
     it "should successfully create a new gram in our database" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+       user = FactoryBot.create(:user)
       sign_in user
       
       post :create, params: { gram: { message: 'Hello!' } }
@@ -51,12 +44,9 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "should properly deal with validation errors" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
+      
       post :create, params: { gram: { message: ''} }
       expect(response).to have_http_status(:unprocessable_entity)
       expect(Gram.count).to eq Gram.count
