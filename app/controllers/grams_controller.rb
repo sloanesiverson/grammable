7db9f648e849c7373aaaaa0dc4b9863dcc6203b0
Sloane <sloanems@aol.com>
2019-10-03 02:32:30 +0000

    secure

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,21 +1,40 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
-  def destroy
+  def index
+  end
+
+  def new
+    @gram = Gram.new
+  end
+
+  def create
+    @gram = current_user.grams.create(gram_params)
+
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
-     @gram.destroy
-     redirect_to root_path
-    end
+  end
 
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+  end
 
-  
   def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
 
     @gram.update_attributes(gram_params)
-    
     if @gram.valid?
       redirect_to root_path
     else
@@ -23,36 +42,14 @@ class GramsController < ApplicationController
     end
   end
 
-  def new
-    @gram = Gram.new
-  end
-
-
-  def index
-
-  end
-
-  def show
+  def destroy
     @gram = Gram.find_by_id(params[:id])
-    if @gram.blank?
-      return render_not_found if @gram.blank?
-    end
-  end
-  def edit
-    @gram = Gram.find_by_id(params[:id])
-    if @gram.blank?
-      return render_not_found if @gram.blank?
-    end
-  end
-
-  def create
-    @gram = current_user.grams.create(gram_params)
-    if @gram.valid?
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+    @gram.destroy
     redirect_to root_path
-  else
-    render :new, status: :unprocessable_entity
-  end 
   end
+
 
   private
 
@@ -61,9 +58,8 @@ class GramsController < ApplicationController
   end
 
 
-  def render_not_found
-    render plain: 'Not Found :(', status: :not_found
+  def render_not_found(status=:not_found)
+    render plain: "#{status.to_s.titleize} :(", status: status
   end
 
 end
-
