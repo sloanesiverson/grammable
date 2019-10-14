a691cdee74a6ca8fc6a4eb9149952cad821cedd1
Sloane <sloanems@aol.com>
2019-10-14 22:08:21 +0000

    almost done

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2019_10_04_003737) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.string "image"
+    t.string "picture"
     t.index ["user_id"], name: "index_grams_on_user_id"
   end
 
